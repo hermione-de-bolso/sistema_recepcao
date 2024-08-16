48d089f428c79f9812231d11b1194ce133f5592a
hermione-de-bolso <nicolegs.ngs@gmail.com>
2024-08-16 13:58:14 -0300

    gcc main.c -o sistema_recepcao

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int numeroVisitantes;
+
+    // Solicitar ao usuário o número de visitantes
+    printf("Digite o número de visitantes do dia: ");
+    scanf("%d", &numeroVisitantes);
+
+    // Exibir mensagem de confirmação
+    printf("Número de visitantes registrado: %d\n", numeroVisitantes);
+
+    return 0;
+}
